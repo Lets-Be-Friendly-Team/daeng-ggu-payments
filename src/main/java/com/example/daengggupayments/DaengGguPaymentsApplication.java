b293d1e6f08ca11b45fcabab6f2e402227c3b3d6
heogeonho <geonh1016@gmail.com>
2024-11-22 16:40:18 +0900

    init: Project 생성

---
+++ src/main/java/com/example/daengggupayments/DaengGguPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.daengggupayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengGguPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaengGguPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daengggupayments/DaengGguPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daengggupayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaengGguPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
