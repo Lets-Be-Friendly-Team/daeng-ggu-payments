dd3b977daae50301654c2b5a280710812f68d8a7
heogeonho <geonh1016@gmail.com>
2024-11-30 00:02:25 +0900

    Chore: 패키지 이름 수정

--- src/main/java/com/ureca/daengggupayments/DaengGguPaymentsApplication.java
+++ src/main/java/com/ureca/daengggupayments/DaengGguPaymentsApplication.java
@@ -1,4 +1,4 @@
-package com.example.daengggupayments;
+package com.ureca.daengggupayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
