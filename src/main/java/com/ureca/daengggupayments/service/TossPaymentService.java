0e26cf40a2ba9d4d70a635c625405f2cb7cf7b37
heogeonho <geonh1016@gmail.com>
2024-12-05 00:10:13 +0900

    Style: 코드 컨벤션 적용

--- src/main/java/com/ureca/daengggupayments/service/TossPaymentService.java
+++ src/main/java/com/ureca/daengggupayments/service/TossPaymentService.java
@@ -5,13 +5,11 @@ import com.ureca.daengggupayments.dto.PaymentResponseDto;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Map;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -24,27 +22,39 @@ public class TossPaymentService {
     public Mono<PaymentResponseDto> confirmPayment(
             String paymentKey, String orderId, BigDecimal amount) {
         return tossPaymentsWebClient
-            .post()
-            .uri("/v1/payments/confirm")
-            .bodyValue(buildPaymentRequest(paymentKey, orderId, amount))
-            .retrieve()
-            .bodyToMono(Map.class)
-            .map(responseMap -> {
-                return PaymentResponseDto.builder()
-                    .paymentKey((String) responseMap.get("paymentKey"))
-                    .orderId((String) responseMap.get("orderId"))
-                    .status((String) responseMap.get("status"))
-                    .totalAmount(new BigDecimal(String.valueOf(responseMap.get("totalAmount"))))
-                    .approvedAt(LocalDateTime.parse((String) responseMap.get("approvedAt")))
-                    .receiptUrl((String) ((Map<?, ?>) responseMap.get("receipt")).get("url"))
-                    .method((String) responseMap.get("method"))
-                    .failure((String) responseMap.get("failure"))
-                    .build();
-            })
-            .doOnError(WebClientResponseException.class, ex -> {
-                log.error("Error occurred while confirming payment. Status: {}, Body: {}",
-                    ex.getStatusCode(), ex.getResponseBodyAsString());
-            });
+                .post()
+                .uri("/v1/payments/confirm")
+                .bodyValue(buildPaymentRequest(paymentKey, orderId, amount))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(
+                        responseMap -> {
+                            return PaymentResponseDto.builder()
+                                    .paymentKey((String) responseMap.get("paymentKey"))
+                                    .orderId((String) responseMap.get("orderId"))
+                                    .status((String) responseMap.get("status"))
+                                    .totalAmount(
+                                            new BigDecimal(
+                                                    String.valueOf(responseMap.get("totalAmount"))))
+                                    .approvedAt(
+                                            LocalDateTime.parse(
+                                                    (String) responseMap.get("approvedAt")))
+                                    .receiptUrl(
+                                            (String)
+                                                    ((Map<?, ?>) responseMap.get("receipt"))
+                                                            .get("url"))
+                                    .method((String) responseMap.get("method"))
+                                    .failure((String) responseMap.get("failure"))
+                                    .build();
+                        })
+                .doOnError(
+                        WebClientResponseException.class,
+                        ex -> {
+                            log.error(
+                                    "Error occurred while confirming payment. Status: {}, Body: {}",
+                                    ex.getStatusCode(),
+                                    ex.getResponseBodyAsString());
+                        });
     }
 
     private PaymentRequestDto buildPaymentRequest(
